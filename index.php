73671cc9242b0d85862d0004596b21b36c86e871
ewallet-thesis <bondadpeter2099@gmail.com>
2021-08-28 11:13:06 +0800

    Test inquire_mobile1

--- index.php
+++ index.php
@@ -46,35 +46,35 @@
     include "transactions/get_balance.php";
     include "transactions/validate_account_replicated.php";
     
-    $route = new Route();
+    // $route = new Route();
     
-    $route->add('/', function(){
-        header("HTTP/1.1 200 Success server connection ");
-        header("Content-Type: application/json");
-        $routes = ['/mobileWallet/signIn', '/mobileWallet/saveAnswers', '/mobileWallet/validateAnswers', '/mobileWallet/registration', '/mobileWallet/getSecurityQuestions','/mobileWallet/inquireMobile','/mobileWallet/inquireEmail','/mobileWallet/requestOTP','/mobileWallet/validateOTP', '/mobileWallet/validateAccount','/mobileWallet/getBalance', '/mobileWallet/FT','/mobileWallet/transactionHistory','/mobileWallet/changeMPIN', '/mobileWallet/deleteAccount'];
-        $response = array();
-        $response['retCode'] = "00";
-        $response['message'] = "Welcome to E-Wallet Open API. See available routes bellow, you can also click to view and test the given routes.";
-        $response['routes'] = $routes;
-        echo json_encode($response);
-    }, null);
-    $route->add('/inquireMobile', 'InquireMobile', $inputs);
-    $route->add('/registration', 'Registration', $inputs);
-    $route->add('/signIn', 'SignIn', $inputs);
-    $route->add('/requestOTP', 'RequestOTP', $inputs);
-    $route->add('/validateOTP', 'ValidateOTP', $inputs);
-    // $route->add('/validateAccount', 'ValidateAccount', $inputs);
-    $route->add('/validateAccount', 'ValidateAccountReplicated', $inputs);
-    $route->add('/FT', 'FundTransfer', $inputs);
-    $route->add('/transactionHistory', 'AccountTransactions', $inputs);
-    $route->add('/getBalance', 'GetBalance', $inputs);
-    $route->add('/getSecurityQuestions', 'SecurityQuestions', $inputs);
-    $route->add('/saveAnswers', 'SaveAnswers', $inputs);
-    $route->add('/validateAnswers', 'ValidateAnswers', $inputs);
-    $route->add('/changeMPIN', 'ChangeMPIN', $inputs);
-    $route->add('/inquireEmail', 'InquireEmail', $inputs);
-    $route->add('/deleteAccount', 'DeleteAccount', $inputs);
+    // $route->add('/', function(){
+    //     header("HTTP/1.1 200 Success server connection ");
+    //     header("Content-Type: application/json");
+    //     $routes = ['/mobileWallet/signIn', '/mobileWallet/saveAnswers', '/mobileWallet/validateAnswers', '/mobileWallet/registration', '/mobileWallet/getSecurityQuestions','/mobileWallet/inquireMobile','/mobileWallet/inquireEmail','/mobileWallet/requestOTP','/mobileWallet/validateOTP', '/mobileWallet/validateAccount','/mobileWallet/getBalance', '/mobileWallet/FT','/mobileWallet/transactionHistory','/mobileWallet/changeMPIN', '/mobileWallet/deleteAccount'];
+    //     $response = array();
+    //     $response['retCode'] = "00";
+    //     $response['message'] = "Welcome to E-Wallet Open API. See available routes bellow, you can also click to view and test the given routes.";
+    //     $response['routes'] = $routes;
+    //     echo json_encode($response);
+    // }, null);
+    // $route->add('/inquireMobile', 'InquireMobile', $inputs);
+    // $route->add('/registration', 'Registration', $inputs);
+    // $route->add('/signIn', 'SignIn', $inputs);
+    // $route->add('/requestOTP', 'RequestOTP', $inputs);
+    // $route->add('/validateOTP', 'ValidateOTP', $inputs);
+    // // $route->add('/validateAccount', 'ValidateAccount', $inputs);
+    // $route->add('/validateAccount', 'ValidateAccountReplicated', $inputs);
+    // $route->add('/FT', 'FundTransfer', $inputs);
+    // $route->add('/transactionHistory', 'AccountTransactions', $inputs);
+    // $route->add('/getBalance', 'GetBalance', $inputs);
+    // $route->add('/getSecurityQuestions', 'SecurityQuestions', $inputs);
+    // $route->add('/saveAnswers', 'SaveAnswers', $inputs);
+    // $route->add('/validateAnswers', 'ValidateAnswers', $inputs);
+    // $route->add('/changeMPIN', 'ChangeMPIN', $inputs);
+    // $route->add('/inquireEmail', 'InquireEmail', $inputs);
+    // $route->add('/deleteAccount', 'DeleteAccount', $inputs);
     
-    $route->submit();
+    // $route->submit();
 
 ?>
